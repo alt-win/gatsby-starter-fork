879c6798d590bc148d06935b713dfeb21a405b7e
Alterntive_Winter <38795952+alt-win@users.noreply.github.com>
2020-08-21 21:44:26 -0700

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import React from 'react'; 
+import React from 'react';
 import logo from '../assets/images/logo.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="BCH DevCon" /></span>
-        <h1>September 5-6</h1>
+        <h1>September 4 - 6</h1>
     </header>
 )
 
-export default Header; 
+export default Header;
